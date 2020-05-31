e2d058d6af8762b5cd750c35c6e2fcae616bc3a5
s19151 <s19151@pjwstk.edu.pl>
2020-05-31 18:15:27 +0200

    Cw12 - Wygenerowanie kontekstu oraz nowego kontrolera. Usnięcie starego kontrolera

---
+++ Cwiczenia12/Cwiczenia12/Controllers/StudentsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cwiczenia12.Models;
+
+namespace Cwiczenia12.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly s19151Context _context;
+
+        public StudentsController(s19151Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Students1
+        public async Task<IActionResult> Index()
+        {
+            var s19151Context = _context.Student.Include(s => s.IdEnrollmentNavigation);
+            return View(await s19151Context.ToListAsync());
+        }
+
+        // GET: Students1/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .Include(s => s.IdEnrollmentNavigation)
+                .FirstOrDefaultAsync(m => m.IndexNumber == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students1/Create
+        public IActionResult Create()
+        {
+            ViewData["IdEnrollment"] = new SelectList(_context.Enrollment, "IdEnrollment", "IdEnrollment");
+            return View();
+        }
+
+        // POST: Students1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IndexNumber,FirstName,LastName,BirthDate,IdEnrollment,Password")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdEnrollment"] = new SelectList(_context.Enrollment, "IdEnrollment", "IdEnrollment", student.IdEnrollment);
+            return View(student);
+        }
+
+        // GET: Students1/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdEnrollment"] = new SelectList(_context.Enrollment, "IdEnrollment", "IdEnrollment", student.IdEnrollment);
+            return View(student);
+        }
+
+        // POST: Students1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("IndexNumber,FirstName,LastName,BirthDate,IdEnrollment,Password")] Student student)
+        {
+            if (id != student.IndexNumber)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.IndexNumber))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdEnrollment"] = new SelectList(_context.Enrollment, "IdEnrollment", "IdEnrollment", student.IdEnrollment);
+            return View(student);
+        }
+
+        // GET: Students1/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .Include(s => s.IdEnrollmentNavigation)
+                .FirstOrDefaultAsync(m => m.IndexNumber == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var student = await _context.Student.FindAsync(id);
+            _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(string id)
+        {
+            return _context.Student.Any(e => e.IndexNumber == id);
+        }
+    }
+}
--- Cwiczenia12/Cwiczenia12/Startup.cs
+++ Cwiczenia12/Cwiczenia12/Startup.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cwiczenia12.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace Cwiczenia12
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<s19151Context>();
+            services.AddScoped<s19151Context, s19151Context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
