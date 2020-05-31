33a9bfbe9990b4deb97d3918251dc1dbc2ea2d7f
s19151 <s19151@pjwstk.edu.pl>
2020-05-31 17:09:34 +0200

    Cw12 - dodanie metody do kontrolera HelloWorldController

--- Cwiczenia12/Cwiczenia12/Controllers/HelloWorldController.cs
+++ Cwiczenia12/Cwiczenia12/Controllers/HelloWorldController.cs
@@ -8,9 +8,16 @@ namespace Cwiczenia12.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public IActionResult Index()
+        //GET: /HelloWorld/
+        public string Index()
         {
-            return View();
+            return "This is the default action";
+        }
+
+        //GET: /HelloWorld/Welcome/
+        public string Welcome() 
+        {
+            return "This is the Welcome action Method";
         }
     }
 }
